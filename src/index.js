2898becc640a2ced36ab63717e6cceee347a87a4
Luffi2539 <vkkovalev91@gmail.com>
2018-09-15 16:49:17 +0300

    almost

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  
+// перевод числа в любую систему счисления
+ /* var numberX=[];
+  var leftover=0;
+    while (number>=base){
+        leftover=number%base;
+        numberX.push(leftover);
+          if(number==base){
+            number=1;
+            break
+          }
+        number=Math.floor(number/base);
+        ;
+        
+      }
+  numberX.push(number);
+  numberX.reverse();
+  numberX=numberX.join(''); 
+  
+  */ 
+var zeros=0;
+var j=2;
+var bases=[];
+for (i=1;i<base;i++){
+    if(base%j==0){
+      bases.push(j);
+      base/=j;
+      }
+    else {
+      j++;
+      
+      }
+  }
+
+bases.push(base);
+  
+var l=bases.length;
+  if(bases[l-1]==1){
+    var newBase=bases[l-2];
+  }
+  else if(bases[l-1]==bases[l-2]){
+    newBase=bases[l-1];
+    var counter=0;
+      for(i=0;i<l;i++){
+        if(bases[i]==newBase){
+        counter++;
+        }
+      }
+    while (Math.floor(number)>0){
+    number/=newBase;
+    zeros+=Math.floor(number);
+    }
+    zeros=Math.floor(zeros/counter);
+    return zeros;
+  }
+  else {
+    newBase=bases[l-1];
+  }
+ 
+while (Math.floor(number)>0){
+  number/=newBase;
+  zeros+=Math.floor(number);
+}
+
+
+return zeros;
 }
